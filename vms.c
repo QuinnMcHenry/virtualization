eca9a7d727009beccdfc12127ea8f24c09373b8b
MaxDeP347 <medepalma@alaska.edu>
2026-04-27 20:56:52 -0800

    added part 4 5 and 6 commands, screenshots for each part and added pipe functionality for communication

--- vms.c
+++ vms.c
@@ -5,10 +5,11 @@
 #include <string.h>
 #include <signal.h>
 
-pid_t vm2_pid = -1;
+pid_t vm_pids[3];
 
 void handler(int sig) {
   printf("VM2 has received the signal from VM1 \n");
+  fflush(stdout);
 }
 
 int checkAllowed(int vmNum, char* path) //vmNum is i+1
@@ -25,9 +26,16 @@ int checkAllowed(int vmNum, char* path) //vmNum is i+1
 }
 
 int main() {
+    vm_pids[1] = 0; // wait for pids to be unset
+    
+    int fd[2];
+    if (pipe(fd) == -1) {
+        perror("Pipe failed");
+        return 1;
+    }
+
     for (int i = 0; i < 3; i++) // will have three children
     {
-    
         char filepath[20]; // we will build path with i using snprintf
         snprintf(filepath, sizeof(filepath), "/mnt/vm%d/hello.txt", i+1);
         
@@ -39,18 +47,25 @@ int main() {
         {
             printf("Child: %d (vm%d)\n", getpid(), i+1);
 
+            sleep(2);
+
             // If VM2 then set signal use handler function
             if (i == 1) {
-              vm2_pid = getpid();
               signal(SIGUSR1, handler);
               printf("VM2 waiting to receive signal \n");
+              fflush(stdout);
+              
+              pause();
+              
             }
 
-            sleep(1); // buffer for VM2 to set up 
-
             if (i == 0) {
+              sleep(3);
               printf("VM1 sending signal to VM2 \n");
-              kill(vm2_pid, SIGUSR1);
+
+              read(fd[0], &vm_pids[1], sizeof(pid_t));
+
+              kill(vm_pids[1], SIGUSR1);
             }
 
             if (checkAllowed(i+1, filepath)) { // check if this child allowed 
@@ -68,12 +83,14 @@ int main() {
                 fclose(fptr); // close before read becasue write may be buffered
 
                 // reading from files
-                fopen(filepath, "r"); 
+                fptr = fopen(filepath, "r"); 
                 while (fgets(buffer, 7, fptr)) {
                     printf("Child %d reading from %s: %s\n", getpid(), filepath, buffer);
                 }
                 fclose(fptr);
             }
+
+            sleep(30);
 
             // demo of vm permissions denial
             switch (i) 
@@ -102,8 +119,17 @@ int main() {
         if (pid > 0) // is the parent
         {
             printf("Parent: %d\n", getpid());
-            wait(NULL); // wait on my childs
+            vm_pids[i] = pid; // store child
+            
+            if (i == 1) { 
+                write(fd[1], &vm_pids[1], sizeof(pid_t));
+            }
         }
-    
     }
+    
+    for (int i = 0; i < 3; i++) {
+       wait(NULL); // wait on my childs
+    }
+
+    return 0;
 }
